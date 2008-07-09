553ca701cc14ca3c98c31876779c3638b07e2a57
admteamkruz <admteamkruz@a3b0ce01-554f-0410-895d-83910fb26d80>
2008-07-09 06:30:53 +0000

    removing test directory

--- trunk/trunk/mokoiax-1.0/python/codegen/scripts/h2xml.py
+++
@@ -1,5 +0,0 @@
-import sys
-from ctypes_codegen.h2xml_main import main
-
-if __name__ == "__main__":
-    sys.exit(main())
--- trunk/trunk/mokoiax-1.0/python/xml2py.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-    from ctypes.wrap.xml2py import main
-    sys.exit(main())
